e992a3ad7c19d709db633780fc1bbfe35dc2edb5
long <phamthanhlong725@gmail.com>
2026-03-03 20:20:20 +0700

    fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,7 +22,7 @@ class AppServiceProvider extends ServiceProvider
     {
         // Trust proxy headers from ngrok and load balancers
         $this->app['request']->setTrustedProxies(['*'], \Illuminate\Http\Request::HEADER_X_FORWARDED_FOR | \Illuminate\Http\Request::HEADER_X_FORWARDED_HOST | \Illuminate\Http\Request::HEADER_X_FORWARDED_PROTO);
-        
+
         // Force HTTPS in production or when behind proxy
         if (env('APP_ENV') === 'production' || request()->header('X-Forwarded-Proto') === 'https') {
             URL::forceScheme('https');
